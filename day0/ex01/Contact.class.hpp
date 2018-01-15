d3edbbb87ae7cd64d02bc11820ca4c9eb98175fa
Aleksey <alekseykres@gmail.com>
2018-01-15 22:14:42 +0200

    add C.c.hpp

---
+++ day0/ex01/Contact.class.hpp
@@ -0,0 +1,30 @@
+#ifndef CONTACT_CLASS_H
+# define CONTACT_CLASS_H
+# include <string>
+# include <iostream>
+
+class Contact {
+	
+	private:
+		std::string	firstName;
+		std::string lastName;
+		std::string nickName;
+		std::string login;
+		std::string postAddress;
+		std::string	phoneNumber;
+		std::string birthday;
+		std::string favoriteMeal;
+		std::string underwearColor;
+		std::string darkestSecret;
+
+	public:
+		Contact(std::string p1, std::string p2, std::string p3, std::string p4,
+				 std::string p5, std::string p6,  std::string p7, std::string p8,
+				 std::string p9, std::string p10);
+
+		~Contact(void);
+
+		void toString();
+		
+};
+#endif
